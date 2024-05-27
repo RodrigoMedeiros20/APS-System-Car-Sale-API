7bd0cb8bbd0ea72c796bc9e7b0cfa7037c44ef62
Rodrigo Medeiros <rodrigo.rmedeiros20@gmail.com>
2024-05-27 20:48:31 -0300

    mais atualizacoes

--- db.js
+++ db.js
@@ -1,17 +1,20 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('database.db'); // Altere para um arquivo persistente
-
-db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS purchases (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nomeCompleto TEXT,
-    cpf TEXT,
-    gmail TEXT,
-    telefone TEXT,
-    cidade TEXT,
-    estado TEXT,
-    carroSelecionado TEXT
-  )`);
+const db = new sqlite3.Database('database.db', (err) => {
+  if (err) {
+    console.error('Erro ao abrir banco de dados:', err.message);
+  } else {
+    console.log('Conectado ao banco de dados SQLite.');
+    db.run(`CREATE TABLE IF NOT EXISTS purchases (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nomeCompleto TEXT,
+      cpf TEXT,
+      gmail TEXT,
+      telefone TEXT,
+      cidade TEXT,
+      estado TEXT,
+      carroSelecionado TEXT
+    )`);
+  }
 });
 
-module.exports = db;
+module.exports = db;
